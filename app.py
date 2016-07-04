eee7f20417959131e3576d02396cc5b0bebbb54b
BTruer <Tarparab@outlook.com>
2016-07-04 19:48:24 -0400

    backend completely functional. post on different buttons work properly

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask, request, render_template
-from logic import getCiphertext
+from logic import getCiphertext, getPlaintext
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    encryptPost = None
+    PostTrue = False
+    textE = ""
+    textD = ""
     if request.method == 'GET':
-        return render_template("index.html", encryptPost=encryptPost)
+        return render_template("index.html", PostTrue=PostTrue, textE=textE, textD=textD)
     if request.method == "POST":
-        plaintext = request.form.get('plaintext')
-        keyE = request.form.get('keyE')
-        encryptPost = getCiphertext(keyE,plaintext)
-        return render_template("index.html", encryptPost=encryptPost)
-
+        if(request.form.get('submitButton') == "encryptMe"):
+            plaintext = request.form.get('plaintext')
+            keyE = request.form.get('keyE')
+            textE = getCiphertext(keyE,plaintext)
+            PostTrue = True
+            return render_template("index.html", PostTrue=PostTrue, textE=textE, textD=textD)
+        elif(request.form.get('submitButton') == "decryptMe"):
+            ciphertext = request.form.get('ciphertext')
+            keyD = request.form.get('keyD')
+            textD = getPlaintext(keyD,ciphertext)
+            return render_template("index.html", PostTrue=True, textE=textE, textD=textD)
 
 
 if __name__ == '__main__':
--- logic.py
+++ logic.py
@@ -1,3 +1,6 @@
 
 def getCiphertext(keyE,plaintext):
     return "this is ciphertext haxs!" + keyE + plaintext
+
+def getPlaintext(keyD,ciphertext):
+    return "plaintextk  " + keyD + ciphertext

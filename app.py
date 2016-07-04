ed3bc660d1f5fb47a65558c2d012736c66120a30
BTruer <Tarparab@outlook.com>
2016-07-04 19:19:04 -0400

    single page render for encryption works!

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask, request, render_template
-
-
-
-
-
+from logic import getCiphertext
 
 app = Flask(__name__)
 
 
-
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    encryptPost = None
     if request.method == 'GET':
-        return render_template("index.html")
+        return render_template("index.html", encryptPost=encryptPost)
     if request.method == "POST":
-        return "hello man"
+        plaintext = request.form.get('plaintext')
+        keyE = request.form.get('keyE')
+        encryptPost = getCiphertext(keyE,plaintext)
+        return render_template("index.html", encryptPost=encryptPost)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ logic.py
@@ -0,0 +1,3 @@
+
+def getCiphertext(keyE,plaintext):
+    return "this is ciphertext haxs!" + keyE + plaintext

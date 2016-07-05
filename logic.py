8577917d7cbe4420a6cdfebe504541338066be5e
Bhargav Tarpara <BTruer@users.noreply.github.com>
2016-07-05 15:55:09 -0400

    wrote the ciphertext method

--- logic.py
+++ logic.py
@@ -1,6 +1,24 @@
-
 def getCiphertext(keyE,plaintext):
-    return "this is ciphertext haxs!" + keyE + plaintext
+    ciphertext=""
+    for char in plaintext:
+        if char.isalpha():
+            if char.islower():
+                num = ord(char)+key
+                if num > 122:
+                    num=num-26
+                    ciphertext = ciphertext + chr(num)
+                else:
+                    ciphertext = ciphertext + chr(num)
+            else:
+                    num = ord(char)+key
+                    if num > 90:
+                        num=num-26
+                        ciphertext = ciphertext + chr(num)
+                    else:
+                        ciphertext = ciphertext + chr(num)
+        else:
+            ciphertext = ciphertext + char
+    return ciphertext
 
 def getPlaintext(keyD,ciphertext):
     return "plaintextk  " + keyD + ciphertext

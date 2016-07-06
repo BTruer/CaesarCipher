87401ff069a3f337c649320986f5e120f5f4b268
BTruer <Tarparab@outlook.com>
2016-07-05 21:23:15 -0400

    finished working application. Added the other function

--- logic.py
+++ logic.py
@@ -3,22 +3,41 @@ def getCiphertext(keyE,plaintext):
     for char in plaintext:
         if char.isalpha():
             if char.islower():
-                num = ord(char)+key
+                num = ord(char)+int(keyE)
                 if num > 122:
                     num=num-26
                     ciphertext = ciphertext + chr(num)
                 else:
                     ciphertext = ciphertext + chr(num)
             else:
-                    num = ord(char)+key
-                    if num > 90:
-                        num=num-26
-                        ciphertext = ciphertext + chr(num)
-                    else:
-                        ciphertext = ciphertext + chr(num)
+                num = ord(char)+int(keyE)
+                if num > 90:
+                    num=num-26
+                    ciphertext = ciphertext + chr(num)
+                else:
+                    ciphertext = ciphertext + chr(num)
         else:
             ciphertext = ciphertext + char
     return ciphertext
 
 def getPlaintext(keyD,ciphertext):
-    return "plaintextk  " + keyD + ciphertext
+    plaintext = ""
+    for char in ciphertext:
+        if char.isalpha():
+            if char.islower():
+                num = ord(char)-int(keyD)
+                if num < 97:
+                    num=num+26
+                    plaintext = plaintext + chr(num)
+                else:
+                    plaintext = plaintext + chr(num)
+            else:
+                num = ord(char)-int(keyD)
+                if num < 65:
+                    num = num+26
+                    plaintext = plaintext + chr(num)
+                else:
+                    plaintext = plaintext + chr(num)
+        else:
+            plaintext = plaintext + char
+    return plaintext
